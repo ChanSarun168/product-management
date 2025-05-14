4c465a4db9a65dd0b0cefeae8036d283a21b75f7
Chan Sarun <chansarun0@gmail.com>
2025-05-14 21:28:13 +0700

    refector:Add Cors to access from frontend

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import { connectDB } from "./configs/db";
 import productRoutes from "./routes/ProductRoute";
 import { studentRoute } from "./routes/category.routes";
 import { errorHandler } from "./middlewares/errorHandler";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors());
+
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
